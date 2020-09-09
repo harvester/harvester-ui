26e196d75f62e8c0343fc5bca93fd96226b2d6b5
DaiYuzeng <yuzeng@rancher.com>
2020-09-09 09:58:22 +0800

    feat(PromptRemove): remove rush batch delete mode

--- store/action-menu.js
+++ store/action-menu.js
@@ -10,7 +10,6 @@ export const state = function() {
     event:            null,
     showPromptRemove: false,
     toRemove:         [],
-    rushRemove:       false,
   };
 };
 
@@ -61,36 +60,21 @@ export const mutations = {
     state.elem = elem;
     state.event = event;
     state.show = true;
-
-    state.rushRemove = false;
-    this.dispatch('listeners/onKeyDown');
   },
 
   hide(state) {
     state.show = false;
     state.resources = null;
     state.elem = null;
-
-    this.dispatch('listeners/offKeyDown');
   },
 
   togglePromptRemove(state, { resources = [], args = {} } = {}) {
-    if (isArray(resources) && resources.length === 0) {
-      state.showPromptRemove = false;
-    } else if (this.getters['listeners/commandDown'] || args.alt) {
-      state.rushRemove = true;
-    } else {
-      state.showPromptRemove = !state.showPromptRemove;
-    }
+    state.showPromptRemove = !state.showPromptRemove;
 
     if (!isArray(resources)) {
       resources = [resources];
     }
     state.toRemove = resources;
-  },
-
-  cleanRushRemove(state) {
-    state.rushRemove = false;
   },
 };
 
