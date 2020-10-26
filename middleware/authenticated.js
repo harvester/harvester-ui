9b5bf2dae7498cbdc09f7ee99e3079b35613a92c
yuzeng <cridequeger@yahoo.com>
2020-10-26 14:37:52 +0800

    Feat: Login page and Authenticated Middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,3 @@
-/* eslint-disable */
-import { NORMAN, MANAGEMENT } from '@/config/types';
-import { findBy } from '@/utils/array';
-import { SETUP } from '@/config/query-params';
 import { get } from '@/utils/object';
 import { ClusterNotFoundError } from '@/utils/error';
 import { applyProducts } from '@/store/type-map';
@@ -27,9 +23,7 @@ function setProduct(store, to) {
   store.commit('setProduct', product);
 }
 
-export default async function({
-  route, app, store, redirect, req, isDev
-}) {
+export default async function({ route, store, redirect }) {
   if ( route.path && typeof route.path === 'string') {
     // Ignore webpack hot module reload requests
     if ( route.path.startsWith('/__webpack_hmr/') ) {
@@ -38,61 +32,6 @@ export default async function({
 
     // Ignore the error page
     if ( route.path.startsWith('/fail-whale') ) {
-      return;
-    }
-  }
-
-  // Initial ?setup=admin-password can technically be on any route
-  const initialPass = route.query[SETUP];
-
-  if ( initialPass ) {
-    const ok = await tryInitialSetup(store, initialPass, isDev);
-
-    if ( ok ) {
-      return redirect(302, `/auth/setup?${ SETUP }=${ escape(initialPass) }`);
-    } else {
-      return redirect(302, '/auth/login');
-    }
-  }
-
-  // Make sure you're actually logged in
-  if ( store.getters['auth/enabled'] !== false && !store.getters['auth/loggedIn'] ) {
-    try {
-      // const principals = await store.dispatch('rancher/findAll', {
-      //   type: NORMAN.PRINCIPAL,
-      //   opt:  {
-      //     url:                  '/v3/principals',
-      //     redirectUnauthorized: false,
-      //   }
-      // });
-
-      // const me = findBy(principals, 'me', true);
-      const me = { id: 'local://user-pq9v5' }; // fake data
-
-      store.commit('auth/hasAuth', true);
-      store.commit('auth/loggedInAs', me.id);
-    } catch (e) {
-      const status = e?._status;
-
-      if ( status === 404 ) {
-        store.commit('auth/hasAuth', false);
-      } else {
-        store.commit('auth/hasAuth', true);
-
-        if ( status === 401 ) {
-          if ( process.env.dev ) {
-            return redirect(302, '/auth/login');
-          } else if ( process.client ) {
-            window.location.href = '/login';
-          } else {
-            return redirect(302, '/login');
-          }
-        } else {
-          store.commit('setError', e);
-          console.log(JSON.stringify(e)); // eslint-disable-line no-console
-        }
-      }
-
       return;
     }
   }
@@ -114,7 +53,7 @@ export default async function({
   }
 
   try {
-    let clusterId = get(route, 'params.cluster');
+    let clusterId = get(route, 'params.cluster') || 'local';
 
     if ( clusterId ) {
       // Run them in parallel
@@ -133,42 +72,11 @@ export default async function({
     }
   } catch (e) {
     if ( e instanceof ClusterNotFoundError ) {
-      return redirect(302, '/clusters');
+      // return redirect(302, '/clusters');
     } else {
       store.commit('setError', e);
 
       return redirect(302, '/fail-whale');
     }
-  }
-}
-
-async function tryInitialSetup(store, password, isDev) {
-  try {
-    const firstLogin = await store.dispatch('management/find', {
-      type: MANAGEMENT.SETTING,
-      id:   'first-login',
-      opt:  { url: `${ MANAGEMENT.SETTING }s/first-login` }
-    });
-
-    if ( isDev ) {
-      // Ignore first-login for dev
-    } else if ( !firstLogin || firstLogin.value !== 'true' ) {
-      // Require first-login to be set for prod
-      return false;
-    }
-
-    const res = await store.dispatch('auth/login', {
-      provider: 'local',
-      body:     {
-        username: 'admin',
-        password
-      },
-    });
-
-    return res === true;
-  } catch (e) {
-    console.error('Error trying initial setup', e); // eslint-disable-line no-console
-
-    return false;
   }
 }
