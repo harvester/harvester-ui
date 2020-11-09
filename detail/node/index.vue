0ab0005830c7c57f704793a8f8d5c271bee38b31
yuzeng <cridequeger@yahoo.com>
2020-11-09 15:48:42 +0800

    Display storage size

--- detail/node/index.vue
+++ detail/node/index.vue
@@ -1,16 +1,13 @@
 <script>
 import Tabbed from '@/components/Tabbed';
 import Tab from '@/components/Tabbed/Tab';
-import Poller from '@/utils/poller';
 import { METRIC, VM, NODE, VMI } from '@/config/types';
 import { HOSTNAME } from '@/config/labels-annotations';
 import { defaultAsyncData } from '@/components/ResourceDetail';
+import { allHash } from '@/utils/promise';
 import Basic from './basic';
 import Instance from './instance';
 // import Monitor from './monitor';
-
-const METRICS_POLL_RATE_MS = 30000;
-const MAX_FAILURES = 2;
 
 export default {
   name: 'DetailNode',
@@ -31,8 +28,18 @@ export default {
   },
 
   async fetch() {
-    const rows = await this.$store.dispatch('cluster/findAll', { type: VM });
+    const hash = {
+      metricNodes: this.$store.dispatch('management/findAll', { type: METRIC.NODE }),
+      nodes:       this.$store.dispatch('management/findAll', { type: NODE }),
+      vms:         this.$store.dispatch('management/findAll', { type: VM }),
+    };
+
+    const res = await allHash(hash);
     const instanceMap = {};
+
+    this.metrics = res.metricNodes.find((item) => {
+      return item.uid === this.value.uid;
+    });
 
     (this.$store.getters['cluster/all'](VMI) || []).forEach((vmi) => {
       const vmiUID = vmi?.metadata?.ownerReferences?.[0]?.uid;
@@ -42,7 +49,7 @@ export default {
       }
     });
 
-    this.rows = rows.filter((row) => {
+    this.rows = res.vms.filter((row) => {
       return instanceMap[row.metadata?.uid]?.status?.nodeName === this.value?.metadata?.labels?.[HOSTNAME];
     });
   },
@@ -67,39 +74,16 @@ export default {
 
   data() {
     return {
-      metricPoller: new Poller(this.loadMetrics, METRICS_POLL_RATE_MS, MAX_FAILURES),
-      metrics:      { cpu: 0, memory: 0 },
+      metrics:      null,
       mode:         'view',
       rows:          []
     };
-  },
-
-  mounted() {
-    this.metricPoller.start();
-  },
-
-  beforeDestroy() {
-    this.metricPoller.stop();
   },
 
   methods: {
     mapToStatus(isOk) {
       return isOk ? 'success' : 'error';
     },
-
-    async loadMetrics() {
-      const schema = this.$store.getters['cluster/schemaFor'](METRIC.NODE);
-
-      if (schema) {
-        await this.$store.dispatch('cluster/find', {
-          type: METRIC.NODE,
-          id:   this.value.id,
-          opt:  { force: true }
-        });
-
-        this.$forceUpdate();
-      }
-    }
   }
 };
 </script>
@@ -107,7 +91,7 @@ export default {
 <template>
   <Tabbed v-bind="$attrs" class="mt-15" :side-tabs="true">
     <Tab name="basic" :label="t('vm.detail.tabs.hostBasic')" :weight="3" class="bordered-table">
-      <Basic v-model="value" :mode="mode" />
+      <Basic v-model="value" :metrics="metrics" :mode="mode" />
     </Tab>
     <Tab name="instance" :label="t('vm.detail.tabs.instance')" :weight="2" class="bordered-table">
       <Instance :rows="rows" />
