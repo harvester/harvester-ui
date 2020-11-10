d9b421ead9069c8a0de3bf8c55ac71cdea655955
wujun <897415845@qq.com>
2020-11-10 09:53:20 +0800

    fix: network catch error

--- components/formatter/ParseNetworkConfig.vue
+++ components/formatter/ParseNetworkConfig.vue
@@ -2,7 +2,7 @@
 export default {
   props:      {
     value: {
-      type:     String,
+      type:     [String, Object],
       default: ''
     },
     col: {
@@ -12,7 +12,14 @@ export default {
   },
   computed: {
     parseValue() {
-      const config = JSON.parse(this.value);
+      let config = {};
+
+      try {
+        config = JSON.parse(this.value);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log('waiting...');
+      }
 
       return config?.[this.col.type] || '';
     }
