12935143601fb8185cfd22a2f0a854103a6256a4
yuzeng <cridequeger@yahoo.com>
2020-11-10 12:27:33 +0800

    Improve roles condition for RKE

--- components/formatter/HostRoles.vue
+++ components/formatter/HostRoles.vue
@@ -1,4 +1,6 @@
 <script>
+import { NODE_ROLES } from '@/config/labels-annotations.js';
+
 export default {
   props: {
     value: {
@@ -12,10 +14,54 @@ export default {
   },
 
   computed: {
-    hostType() {
-      const isMaster = this.value['node-role.kubernetes.io/master'] === 'true';
+    isWorker() {
+      const { WORKER: worker } = NODE_ROLES;
 
-      return isMaster ? this.t('node.detail.type.management') : this.t('node.detail.type.compute');
+      return `${ this.value[worker] }` === 'true';
+    },
+
+    isControlPlane() {
+      const { CONTROL_PLANE: controlPlane } = NODE_ROLES;
+
+      return `${ this.value[controlPlane] }` === 'true';
+    },
+
+    isEtcd() {
+      const { ETCD: etcd } = NODE_ROLES;
+
+      return `${ this.value[etcd] }` === 'true';
+    },
+
+    isK3sMaster() {
+      return this.value['node-role.kubernetes.io/master'] === 'true';
+    },
+
+    hostType() {
+      const management = this.t('node.detail.type.management');
+      const compute = this.t('node.detail.type.compute');
+      const all = 'All';
+      const isWorker = this.isWorker;
+      const isControlPlane = this.isControlPlane;
+      const isEtcd = this.isEtcd;
+      const isK3sMaster = this.isK3sMaster;
+
+      if (!isWorker && !isControlPlane && !isEtcd && isK3sMaster) {
+        return management;
+      }
+
+      if (isWorker && isControlPlane && isEtcd) {
+        return all;
+      }
+
+      if (isControlPlane || isEtcd) {
+        return management;
+      }
+
+      if (isWorker) {
+        return compute;
+      }
+
+      return compute;
     }
   },
 
