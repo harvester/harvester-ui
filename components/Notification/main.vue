3c3adf5ee53fed3e24274d447ce9197465bd92ea
yuzeng <cridequeger@yahoo.com>
2020-11-11 13:50:28 +0800

    Add Notification

---
+++ components/Notification/main.vue
@@ -0,0 +1,225 @@
+<script>
+const typeMap = {
+  success: 'success',
+  info:    'info',
+  warning: 'warning',
+  error:   'error'
+};
+
+export default {
+  data() {
+    return {
+      visible:        false,
+      message:        '',
+      title:          '',
+      duration:       4500,
+      onClose:        null,
+      onClick:        null,
+      closed:         false,
+      timer:          null,
+      position:       'top-right',
+      verticalOffset: 0
+    };
+  },
+
+  computed: {
+    typeClass() {
+      return this.type && typeMap[this.type] ? `icon-h-${ typeMap[this.type] } color-${ typeMap[this.type] }` : '';
+    },
+
+    horizontalClass() {
+      return this.position.includes('right') ? 'right' : 'left';
+    },
+
+    verticalProperty() {
+      return this.position.includes('top') ? 'top' : 'bottom';
+    },
+
+    positionStyle() {
+      return { [this.verticalProperty]: `${ this.verticalOffset }px` };
+    }
+  },
+
+  watch: {
+    closed(newVal) {
+      if (newVal) {
+        this.visible = false;
+      }
+    }
+  },
+
+  mounted() {
+    if (this.duration > 0) {
+      this.timer = setTimeout(() => {
+        if (!this.closed) {
+          this.close();
+        }
+      }, this.duration);
+    }
+    document.addEventListener('keydown', this.keydown);
+  },
+
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.keydown);
+  },
+
+  methods: {
+    destroyElement() {
+      this.$destroy(true);
+      this.$el.parentNode.removeChild(this.$el);
+    },
+
+    click() {
+      if (typeof this.onClick === 'function') {
+        this.onClick();
+      }
+    },
+
+    close() {
+      this.closed = true;
+      if (typeof this.onClose === 'function') {
+        this.onClose();
+      }
+    },
+
+    clearTimer() {
+      clearTimeout(this.timer);
+    },
+
+    startTimer() {
+      if (this.duration > 0) {
+        this.timer = setTimeout(() => {
+          if (!this.closed) {
+            this.close();
+          }
+        }, this.duration);
+      }
+    },
+
+    keydown(e) {
+      if (e.keyCode === 46 || e.keyCode === 8) {
+        this.clearTimer();
+      } else if (e.keyCode === 27) { // Close by ESC
+        if (!this.closed) {
+          this.close();
+        }
+      } else {
+        this.startTimer();
+      }
+    },
+
+  }
+};
+</script>
+
+<template>
+  <transition
+    name="slide-fade"
+    @after-leave="destroyElement"
+  >
+    <div
+      v-show="visible"
+      :class="['h-notification', horizontalClass]"
+      :style="positionStyle"
+      @mouseenter="clearTimer()"
+      @mouseleave="startTimer()"
+      @click="click"
+    >
+      <div class="h-notification__icon">
+        <i :class="['icons', 'iconfont', typeClass]"></i>
+      </div>
+      <div class="h-notification__group">
+        <h3 v-if="title" class="h-notification__title">
+          {{ title }}
+        </h3>
+        <div class="h-notification__message">
+          <p>{{ message }}</p>
+        </div>
+        <div class="h-notification__close" @click.stop="close">
+          <i class="icon icon-fw icon-close"></i>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
+
+    &.left {
+      transform: translateX(-10px);
+    }
+  }
+
+  .h-notification {
+    display: flex;
+    box-sizing: border-box;
+    position: fixed;
+    border: 1px solid #ebeef5;
+    border-radius: 8px;
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+    width: 330px;
+    padding: 12px 26px 14px 13px;
+    background: var(--body-bg);
+    transition:opacity .3s,transform .3s,left .3s,right .3s,top .4s,bottom .3s;
+
+    &__icon {
+      font-size: 24px;
+      width: 24px;
+      margin-right: 11px;
+    }
+
+    &__group {
+      flex: 1;
+    }
+
+    &__title {
+      margin-top: 2px;
+    }
+
+    &__message {
+      p {
+        font-size: 1.2em;
+        word-break: break-all;
+      }
+    }
+
+    &__close {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      font-size: 18px;
+      cursor: pointer;
+    }
+
+    &.right {
+      right: 15px;
+    }
+
+    &.left {
+      left: 15px;
+    }
+
+    .icon-h-success {
+      color: var(--success);
+    }
+
+    .icon-h-info {
+      color: var(--info);
+    }
+
+    .icon-h-warning {
+      color: var(--warning);
+    }
+
+    .icon-h-error {
+      color: var(--error);
+    }
+  }
+</style>
