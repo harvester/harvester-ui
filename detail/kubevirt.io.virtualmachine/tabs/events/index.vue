b892b402c6d2a37f1c0075597395b4647a587927
yuzeng <cridequeger@yahoo.com>
2020-11-30 15:34:12 +0800

    Use SortableTable to display VM Events

--- detail/kubevirt.io.virtualmachine/tabs/events/index.vue
+++ detail/kubevirt.io.virtualmachine/tabs/events/index.vue
@@ -1,19 +1,13 @@
 <script>
-import ResourceState from '../../resource-state/index';
+import { REASON } from '@/config/table-headers';
+import SortableTable from '@/components/SortableTable';
 
 export default {
   name: 'Events',
 
-  components: { ResourceState },
+  components: { SortableTable },
 
   props: {
-    resource: {
-      type:     Object,
-      required: true,
-      default:  () => {
-        return {};
-      }
-    },
     events: {
       type:     Array,
       required: true,
@@ -21,97 +15,61 @@ export default {
   },
 
   data() {
-    return {};
+    const reason = {
+      ...REASON,
+      ...{ canBeVariable: true },
+      width: 100
+    };
+
+    const eventHeaders = [
+      reason,
+      {
+        name:          'resource',
+        label:         'Resource',
+        labelKey:      'clusterIndexPage.sections.events.resource.label',
+        value:         'displayInvolvedObject',
+        sort:          ['involvedObject.kind', 'involvedObject.name'],
+        canBeVariable: true,
+      },
+      {
+        align:         'right',
+        name:          'date',
+        label:         'Date',
+        labelKey:      'clusterIndexPage.sections.events.date.label',
+        value:         'lastTimestamp',
+        sort:          'lastTimestamp:desc',
+        formatter:     'LiveDate',
+        formatterOpts: { addSuffix: true },
+        width:         125,
+        defaultSort:   true,
+      },
+    ];
+
+    return { eventHeaders };
   },
 
-  computed: {
-    isNS() {
-      return 'Namespace';
-    },
-    isDown() {
-      return this.isEmpty(this.resource);
-    },
-  },
-
-  methods: {
-    getEventTime(time) {
-      const date = new Date(time);
-      let hours = date.getHours();
-      let minutes = date.getMinutes();
-
-      if (hours < 10) {
-        hours = `0${ hours }`;
-      }
-
-      if (minutes < 10) {
-        minutes = `0${ minutes }`;
-      }
-
-      return `${ hours }:${ minutes }`;
-    },
-    isEmpty(o) {
-      return o !== undefined && Object.keys(o).length === 0;
-    }
-  }
 };
 </script>
 
 <template>
-  <div class="vm-events">
-    <div v-if="events.length > 0">
-      <div v-for="(event, index) in events" :key="event.id">
-        <div class="row">
-          <div class="col span-4">
-            <ResourceState v-model="event.involvedObject.kind" />
-            <ResourceState v-if="event._type !== 'Normal'" v-model="event._type" />
-          </div>
-          <div class="col span-4 text-center">
-            <span class="vm-events__reason">{{ event.reason }}</span>
-          </div>
-          <div class="col span-4 text-right">
-            {{ getEventTime(event.lastTimestamp) }}
-          </div>
-          <!-- <div class="col-12"></div> -->
-        </div>
-        <div class="row mt-10">
-          <div class="col span-12">
-            {{ t("vm.detail.events.from") }} {{ event.source.component }}
-          </div>
-        </div>
-        <div class="row mt-10">
-          <div class="col span-12">
-            {{ event.message }}
-          </div>
-        </div>
-        <hr v-if="index < events.length - 1" class="section-divider" />
+  <SortableTable
+    :rows="events"
+    :headers="eventHeaders"
+    key-field="id"
+    :search="false"
+    :table-actions="false"
+    :row-actions="false"
+    :paging="true"
+    :rows-per-page="10"
+    default-sort-by="date"
+  >
+    <template #cell:resource="{row, value}">
+      <div class="text-info">
+        {{ value }}
       </div>
-    </div>
-    <p v-else>
-      {{ t("vm.detail.events.down") }}
-    </p>
-  </div>
+      <div v-if="row.message">
+        {{ row.displayMessage }}
+      </div>
+    </template>
+  </SortableTable>
 </template>
-
-<style lang="scss" scoped>
-  .vm-events {
-    .el-card {
-      margin-bottom: 15px;
-
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
-
-    .badge-state {
-      padding: 2px 5px;
-      margin-right: 3px;
-    }
-
-    &__reason {
-      border: 1px solid var(--input-label);
-      border-radius: 3px;
-      padding: 0 3px;
-      color: var(--input-label);
-    }
-  }
-</style>
