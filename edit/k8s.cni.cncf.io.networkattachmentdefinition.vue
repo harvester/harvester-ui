1d77e9e7bd42c65ef3b4df9a01ec1bcb9a0cf26e
wujun <897415845@qq.com>
2020-12-09 17:31:30 +0800

    fix: finash save

--- edit/k8s.cni.cncf.io.networkattachmentdefinition.vue
+++ edit/k8s.cni.cncf.io.networkattachmentdefinition.vue
@@ -118,7 +118,7 @@ export default {
     :mode="mode"
     :errors="errors"
     @apply-hooks="applyHooks"
-    @finish="save"
+    @finish="beforeSave"
   >
     <div>
       <LabeledInput v-model="name" class="mb-20" label="Name" required :disabled="isEdit" />
