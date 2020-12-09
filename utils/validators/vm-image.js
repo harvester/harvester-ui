c2662a760187626374728c5c8d9b1900766242b0
yuzeng <cridequeger@yahoo.com>
2020-12-09 14:42:11 +0800

    VM Image custom validations

---
+++ utils/validators/vm-image.js
@@ -0,0 +1,17 @@
+export function imageUrl(url, getters, errors, validatorArgs) {
+  if (!url || url === '') {
+    errors.push(getters['i18n/t']('validation.required', { key: 'Url' }));
+
+    return errors;
+  }
+
+  const suffixName = url.split('/').pop();
+  const fileSuffiic = suffixName.split('.').pop();
+  const filesFormat = ['gz', 'qcow', 'qcow2', 'raw', 'img', 'xz', 'iso'];
+
+  if (!filesFormat.includes(fileSuffiic)) {
+    errors.push('The URL you have entered ends in an extension that we do not support. We only accept image files that end in .img, .iso, .qcow2, .raw, and compressed (.tar, .gz, .xz) of the above formats).');
+  }
+
+  return errors;
+}
