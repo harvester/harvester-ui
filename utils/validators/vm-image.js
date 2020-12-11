3b64e2dc7d66d02a79ed3e5de1a0e2569e96169b
yuzeng <cridequeger@yahoo.com>
2020-12-11 16:38:01 +0800

    convert file suffic of image to lower case

--- utils/validators/vm-image.js
+++ utils/validators/vm-image.js
@@ -6,7 +6,7 @@ export function imageUrl(url, getters, errors, validatorArgs) {
   }
 
   const suffixName = url.split('/').pop();
-  const fileSuffiic = suffixName.split('.').pop();
+  const fileSuffiic = suffixName.split('.').pop().toLowerCase();
   const filesFormat = ['gz', 'qcow', 'qcow2', 'raw', 'img', 'xz', 'iso'];
 
   if (!filesFormat.includes(fileSuffiic)) {
