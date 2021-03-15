819f7053861dc67ab3441b4dc742f5abd2f7efae
wujun <897415845@qq.com>
2021-03-15 17:06:31 +0800

    fix: change nodenetowrk labels

---
+++ detail/network.harvester.cattle.io.clusternetwork.vue
@@ -0,0 +1,56 @@
+<script>
+import Tabbed from '@/components/Tabbed';
+import Tab from '@/components/Tabbed/Tab';
+import LabelValue from '@/components/LabelValue';
+import RadioGroup from '@/components/form/RadioGroup';
+
+export default {
+  name: 'Detail',
+
+  components: {
+    Tab,
+    Tabbed,
+    LabelValue,
+    RadioGroup
+  },
+
+  props: {
+    value: {
+      type:     Object,
+      required: true,
+    }
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <Tabbed id="clusterNetworkDetail" v-bind="$attrs" class="mt-15" :side-tabs="true">
+    <Tab name="basics" :label="t('harvester.vmPage.detail.tabs.basics')" class="bordered-table">
+      <RadioGroup
+        v-model="value.enable"
+        :disabled="true"
+        class="mb-20"
+        name="model"
+        :options="[true,false]"
+        :labels="['Enable', 'Disable']"
+      />
+
+      <LabelValue v-if="value.enable" name="DefaultPhysicalNIC" :value="value.config.defaultPhysicalNIC" />
+    </Tab>
+  </Tabbed>
+</template>
+
+<style lang="scss">
+#clusterNetworkDetail {
+  .radio-group {
+    display: flex;
+    .radio-container {
+      margin-right: 30px;
+    }
+  }
+}
+</style>
