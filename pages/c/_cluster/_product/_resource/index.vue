b9ad8b4605d11a1f562137daba1ef300eea645e2
wujun <897415845@qq.com>
2021-03-19 15:16:47 +0800

    fix: 527

--- pages/c/_cluster/_product/_resource/index.vue
+++ pages/c/_cluster/_product/_resource/index.vue
@@ -4,9 +4,26 @@ import ResourceList from '@/components/ResourceList';
 export default {
   name:       'ClusterResourcedList',
   components: { ResourceList },
+
+  fetch() {
+    const store = this.$store;
+    const params = { ...this.$route.params };
+    const resource = params.resource;
+    const inStore = store.getters['currentProduct'].inStore;
+    const schema = store.getters[`${ inStore }/schemaFor`](resource);
+
+    if (schema) {
+      this.hasResource = true;
+    }
+  },
+
+  data() {
+    return { hasResource: false };
+  }
 };
 </script>
 
 <template>
-  <ResourceList />
+  <ResourceList v-if="hasResource" />
+  <div v-else></div>
 </template>
