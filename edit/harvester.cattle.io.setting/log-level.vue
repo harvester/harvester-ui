7c3781c5ec30af0b23dfe00334768159af4f90d1
wujun <897415845@qq.com>
2021-04-08 11:03:52 +0800

    feat: add log level configuration

---
+++ edit/harvester.cattle.io.setting/log-level.vue
@@ -0,0 +1,48 @@
+<script>
+import LabeledSelect from '@/components/form/LabeledSelect';
+
+export default {
+  name:       'LogLevel',
+  components: { LabeledSelect },
+
+  props:      {
+    value: {
+      type:     Object,
+      required: true,
+    }
+  },
+
+  data() {
+    const logLevel = this.value.value || this.value.default;
+
+    return { logLevel };
+  },
+
+  computed: {
+    logOption() {
+      return [{
+        label: 'Info',
+        value: 'info'
+      }, {
+        label: 'Debug',
+        value: 'debug'
+      }, {
+        label: 'Trace',
+        value: 'trace'
+      }];
+    }
+  },
+
+  methods: {
+    update() {
+      this.$set(this.value, 'value', this.logLevel);
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <LabeledSelect v-model="logLevel" class="mb-20" :label="t('generic.value')" :options="logOption" @input="update" />
+  </div>
+</template>
