0794cf41c0e1b526cae7ca5b0a42c6eb5a69aa5e
wujun <897415845@qq.com>
2021-04-08 13:12:15 +0800

    fix: vm state error(add pod status)

--- models/pod.js
+++ models/pod.js
@@ -1,6 +1,20 @@
+import _ from 'lodash';
 import { findBy, insertAt } from '@/utils/array';
 import { colorForState, stateDisplay } from '@/plugins/steve/resource-instance';
 import { NODE, WORKLOAD_TYPES } from '@/config/types';
+
+const POD_STATUS_NOT_SCHEDULABLE = 'POD_NOT_SCHEDULABLE';
+
+const POD_STATUS_FAILED = 'POD_FAILED';
+const POD_STATUS_CRASHLOOP_BACKOFF = 'POD_CRASHLOOP_BACKOFF';
+const POD_STATUS_UNKNOWN = 'POD_STATUS_UNKNOWN';
+const POD_STATUS_CONTAINER_FAILING = 'POD_CONTAINER_FAILING';
+const POD_STATUS_NOT_READY = 'POD_NOT_READY';
+
+const POD_STATUS_PENDING = 'POD_STATUS_PENDING';
+const POD_STATUS_COMPLETED = 'POD_STATUS_COMPLETED';
+const POD_STATUS_SUCCEEDED = 'POD_STATUS_SUCCEEDED';
+const POD_STATUS_RUNNING = 'POD_STATUS_RUNNING';
 
 export const WORKLOAD_PRIORITY = {
   [WORKLOAD_TYPES.DEPLOYMENT]:             1,
@@ -10,6 +24,27 @@ export const WORKLOAD_PRIORITY = {
   [WORKLOAD_TYPES.JOB]:                    5,
   [WORKLOAD_TYPES.REPLICA_SET]:            6,
   [WORKLOAD_TYPES.REPLICATION_CONTROLLER]: 7,
+};
+
+const failedWaitingContainerReasons = ['ImagePullBackOff', 'ErrImagePull', 'CrashLoopBackOff'];
+const failedTerminationContaineReasons = ['Error'];
+
+const errorStatusMapper = {
+  Failed:           POD_STATUS_FAILED,
+  CrashLoopBackOff: POD_STATUS_CRASHLOOP_BACKOFF,
+  Unknown:          POD_STATUS_UNKNOWN,
+};
+
+const okStatusMapper = {
+  Pending:   POD_STATUS_PENDING,
+  Running:   POD_STATUS_RUNNING,
+  Completed: POD_STATUS_COMPLETED,
+  Succeeded: POD_STATUS_SUCCEEDED,
+};
+
+const stateReasonResolver = {
+  terminated: ({ reason, exitCode }) => `Terminated with ${ reason }${ exitCode ? ` (exit code ${ exitCode }).` : '.' }`,
+  waiting:    ({ reason }) => `Waiting (${ reason }).`,
 };
 
 export default {
@@ -160,5 +195,143 @@ export default {
 
   isRunning() {
     return this.status.phase === 'Running';
-  }
+  },
+
+  nodes() {
+    return this.$rootGetters['cluster/all'](NODE);
+  },
+
+  node() {
+    const { nodeName } = this.spec;
+
+    return this.nodes.filter((node) => {
+      return node?.metadata?.name === nodeName;
+    })[0];
+  },
+
+  getPodStatus() {
+    return this.isNotSchedulable ||
+    this.hasErrorStatus ||
+    this.isContainerFailing ||
+    this.isNotReady ||
+    this.hasOkStatus || { status: POD_STATUS_UNKNOWN };
+  },
+
+  isNotSchedulable() {
+    if (!this.isPodSchedulable) {
+      return {
+        status:  POD_STATUS_NOT_SCHEDULABLE,
+        message: 'Pod scheduling failed.',
+      };
+    }
+
+    return null;
+  },
+
+  hasErrorStatus() {
+    const status = errorStatusMapper[this.getStatusPhase];
+
+    if (status) {
+      return {
+        status,
+        message: this.getContainerStatusReason(this.findFailingContainerStatus),
+      };
+    }
+
+    return null;
+  },
+
+  isPodSchedulable() {
+    const podScheduledCond = this.getStatusConditionOfType('PodScheduled');
+
+    return !(
+      podScheduledCond &&
+      podScheduledCond.status !== 'True' &&
+      podScheduledCond.reason === 'Unschedulable'
+    );
+  },
+
+  findFailingContainerStatus() {
+    return _.get(this, 'status.containerStatuses', []).find((container) => {
+      return !container.ready &&
+      (_.includes(failedWaitingContainerReasons, _.get(container, 'state.waiting.reason')) ||
+      _.includes(failedTerminationContaineReasons, _.get(container, 'state.terminated.reason')));
+    });
+  },
+
+  getContainerStatusReason() {
+    return (containerStatus) => {
+      if (containerStatus) {
+        const stateName = Object.getOwnPropertyNames(containerStatus.state).find(
+          pn => !!containerStatus.state[pn].reason,
+        );
+
+        if (stateName) {
+          const state = containerStatus.state[stateName];
+
+          return (
+            state.message ||
+            (stateReasonResolver[stateName] && stateReasonResolver[stateName](state)) ||
+            stateName
+          );
+        }
+      }
+
+      return undefined;
+    };
+  },
+
+  isContainerFailing() {
+    const failingContainer = this.findFailingContainerStatus;
+
+    if (failingContainer) {
+      return {
+        status:  POD_STATUS_CONTAINER_FAILING,
+        message: this.getContainerStatusReason(failingContainer),
+      };
+    }
+
+    return null;
+  },
+
+  isNotReady() {
+    const message = this.findPodFalseStatusConditionMessage;
+
+    if (message) {
+      return {
+        status: POD_STATUS_NOT_READY,
+        message,
+      };
+    }
+
+    return null;
+  },
+
+  hasOkStatus() {
+    const status = okStatusMapper[this.getStatusPhase];
+
+    if (status) {
+      return { status };
+    }
+
+    return null;
+  },
+
+  findPodFalseStatusConditionMessage() {
+    const notReadyConditions = this.getPodFalseStatusConditions;
+
+    if (notReadyConditions.length > 0) {
+      return notReadyConditions[0].message || `Step: ${ notReadyConditions[0].type }`;
+    }
+
+    return undefined;
+  },
+
+  getPodFalseStatusConditions() {
+    return this.getPodStatusConditions.filter(condition => condition.status !== 'True');
+  },
+
+  getPodStatusConditions() {
+    return _.get(this, 'status.conditions', []);
+  },
 };
