7efd16015423c73bce5934f25940833e430ff43c
wujun <897415845@qq.com>
2021-04-19 14:39:58 +0800

    feat: vm status detail message

--- components/formatter/vmState.vue
+++ components/formatter/vmState.vue
@@ -43,15 +43,33 @@ export default {
       return (!!nn?.message || !this.enableClusterNetwork) && this.row?.attachNetwork && this?.row?.actualState === 'Running';
     },
 
-    message() {
-      return this.t('harvester.networkPage.message.vlanInactive', { name: this.row.realAttachNodeName });
-    },
-
     enableClusterNetwork() {
       const clusterNetwork = this.allClusterNetwork?.[0] || {};
 
       return clusterNetwork?.enable;
     },
+
+    warningMessage() {
+      const out = [];
+
+      if (this.NetworkImpassability) {
+        out.push(this.t('harvester.networkPage.message.vlanInactive', { name: this.row.realAttachNodeName }));
+      }
+
+      if (this.row.warningMessage?.pod) {
+        const pod = this.row.warningMessage?.pod;
+
+        if (pod.metadata?.state?.error && !/pod has unbound immediate PersistentVolumeClaims/.test(pod.metadata?.state?.message)) {
+          out.push(pod.metadata?.state?.message);
+        }
+      }
+
+      if (this.row?.actualState === 'VM error' && this.row.warningMessage?.message) {
+        out.push(this.row.warningMessage?.message);
+      }
+
+      return out;
+    }
   },
 
   methods: {
@@ -68,7 +86,21 @@ export default {
     <MigrationState v-show="isMigrating" :vm-resource="row" @state-changed="migrationStateChanged" />
     <div v-show="!isMigrating" class="state">
       <VMState :row="row" />
-      <i v-if="NetworkImpassability" v-tooltip="message" class="icon icon-warning icon-lg text-warning" />
+      <v-popover
+        v-if="warningMessage.length"
+        trigger="hover"
+        offset="16"
+      >
+        <span class="tooltip-target">
+          <i class="icon icon-warning icon-lg text-warning" />
+        </span>
+
+        <template slot="popover">
+          <p v-for="(message, index) in warningMessage" :key="message">
+            {{ index+1 }}: {{ message }}
+          </p>
+        </template>
+      </v-popover>
     </div>
   </span>
 </template>
