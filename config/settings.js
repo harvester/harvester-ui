b07486a1f09256a8c543cc4ab156acf822f0ba2e
n313893254 <n313893254@126.com>
2021-08-10 17:20:21 +0800

    HARVESTER: Add manager setting when single virtual cluster

--- config/settings.js
+++ config/settings.js
@@ -123,7 +123,6 @@ export const HCI_ALLOWED_SETTINGS = {
   [HCI_SETTING.VLAN]:                             {
     kind: 'custom', from: 'import', alias: 'vlan'
   },
-  // [HCI_SETTING.DEFAULT_STORAGE_CLASS]: {}
 };
 
 export const fetchOrCreateSetting = async(store, id, val, save = true) => {
